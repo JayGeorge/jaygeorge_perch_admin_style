b6d7d620b28df5add929b8d5bbbfe0fe318fcf65
Jay George <contact@jaygeorge.co.uk>
2019-11-09 19:56:50 +0000

    Perch Admin Style -- update focus colour

--- standard-admin.js
+++ standard-admin.js
@@ -5,4 +5,4 @@
     - Add a simple Skip Link to get to main content quickly
 
 */
-document.querySelector('html').insertAdjacentHTML('afterbegin', '<a href="#main" class="c-skip-to-content" title="Skip to content">Skip to content</a>');
+document.querySelector('html').insertAdjacentHTML('afterbegin', '<a href="#content-edit" class="c-skip-to-content" title="Skip to content">Skip to content</a><a href="#main" class="c-skip-to-content" title="Skip to main">Skip to main</a>');
