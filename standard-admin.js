4e41d6bb78154eeb00784412a4b696a602266938
Jay George <contact@jaygeorge.co.uk>
2019-11-09 20:22:19 +0000

    Accessibility -- Dynamically create a Skip Link for editable content

--- standard-admin.js
+++ standard-admin.js
@@ -2,7 +2,15 @@
 =================================================== */
 /* Notes...
 
-    - Add a simple Skip Link to get to main content quickly
+    - Add some Skip Links to get to main content quickly
 
 */
-document.querySelector('html').insertAdjacentHTML('afterbegin', '<a href="#content-edit" class="c-skip-to-content" title="Skip to content">Skip to content</a><a href="#main" class="c-skip-to-content" title="Skip to main">Skip to main</a>');
+// Always add 'Skip to main'
+document.querySelector('html').insertAdjacentHTML('afterbegin', '<a href="#main" class="c-skip-to-content" title="Skip to main">Skip to main</a>');
+
+// If there is editable content i.e. we're not on something like 'Pages'
+if(document.getElementById('content-edit')){
+    // [1] Let's get past the first divider, so add an ID to the first element after that 
+    document.querySelector('#content-edit h2 + *').id = 'editable-content';
+    document.querySelector('html').insertAdjacentHTML('afterbegin', '<a href="#editable-content" class="c-skip-to-content" title="Skip to content">Skip to content</a>');
+}
